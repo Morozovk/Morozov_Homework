d389eb686a94109024761e1384a4c7bbb85a89dd
Кирилл <106439395+Morozovk@users.noreply.github.com>
2025-02-27 23:53:05 +0300

    замена текста для конфликта

--- src/main/java/com/morozov/Main.java
+++ src/main/java/com/morozov/Main.java
@@ -3,10 +3,8 @@ package com.morozov;
 public class Main {
     public static void main(String[] args) {
 
-
-        System.out.printf(getMessage());
         System.out.println(getMessage());
-        System.out.println(getMessage());
+        System.out.println("Тестирую конфликт")
     }
     private static String getMessage(){return "Hello qa !!!";}
 }
