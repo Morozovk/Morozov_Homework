85b36ec1608473ea6e959ae5628295269cb29caa
Morozov Kirill <k.morozov@magdv.com>
2025-02-28 00:10:07 +0300

    Добавлен вывод hello qa и текст

--- src/main/java/com/morozov/Main.java
+++ src/main/java/com/morozov/Main.java
@@ -4,6 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf(getMessage());
+
+
+        System.out.println("текст текст текст");
+
+        System.out.printf(getMessage());
+        System.out.printf(getMessage());
+        System.out.printf(getMessage());
+        System.out.printf(getMessage());
     }
     private static String getMessage(){return "Hello qa !!!";}
 }
