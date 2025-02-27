02037779611bb6f4ba6c44fec54fa74275552549
Morozov Kirill <k.morozov@magdv.com>
2025-02-28 00:07:18 +0300

    добавлен вывод строки тест

--- src/main/java/com/morozov/Main.java
+++ src/main/java/com/morozov/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf(getMessage());
+        System.out.println("тест тест тест");
     }
     private static String getMessage(){return "Hello qa !!!";}
 }
