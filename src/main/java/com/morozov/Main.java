f3e484c6fd0a143aa50114e6f9823e580ff0b1ca
Morozov Kirill <k.morozov@magdv.com>
2025-02-28 00:03:00 +0300

    убрал текст

--- src/main/java/com/morozov/Main.java
+++ src/main/java/com/morozov/Main.java
@@ -3,10 +3,7 @@ package com.morozov;
 public class Main {
     public static void main(String[] args) {
 
-        String test = 5;
         System.out.printf(getMessage());
-        System.out.println(getMessage());
-        System.out.println(getMessage());
     }
     private static String getMessage(){return "Hello qa !!!";}
 }
