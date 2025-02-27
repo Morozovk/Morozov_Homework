84e8debf892865c815b5af441b60cfcd1055e6a7
Morozov Kirill <k.morozov@magdv.com>
2025-02-27 21:06:58 +0300

    new code

--- src/main/java/com/morozov/Main.java
+++ src/main/java/com/morozov/Main.java
@@ -3,6 +3,7 @@ package com.morozov;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello world");
+        System.out.printf(getMessage());
     }
+    private static String getMessage(){return "Hello qa !!!";}
 }
